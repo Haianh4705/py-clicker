f185149f7dbf026a99b1fa651b7bc0a3c6768655
Haianh <haianhnguyen472005@gmail.com>
2023-09-07 00:02:20 +0700

    update img and point

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame, sys, random
 
-WINDOW_SIZE = WIN_W, WIN_H = (1280, 720)
-DISPLAY_SIZE = W, H = (640, 360)
+WINDOW_SIZE = W, H = (1280, 720)
 BLUE = (67, 156, 239)
+BROWN = (101,83,83)
 FPS = 60
+SPAWNTARGET = pygame.USEREVENT + 1
 
 target_img = pygame.transform.scale_by(pygame.image.load('data/target.png'), 3)
 
@@ -14,13 +15,20 @@ class Target(pygame.sprite.Sprite):
         self.pos = pos
         self.image = target_img.convert_alpha()
         self.rect = self.image.get_rect(center = self.pos)
+        self.clicked = False
+        self.init_time = pygame.time.get_ticks()
+    
+    def timer(self):
+        if pygame.time.get_ticks() - self.init_time > 1000:
+            self.kill()
     
     def click(self):
         if pygame.mouse.get_pressed()[0]:
             if self.rect.collidepoint(pygame.mouse.get_pos()):
-                self.kill()
+                self.clicked = True
     
     def update(self):
+        self.timer()
         self.click()
 
 class Game:
@@ -31,8 +39,13 @@ class Game:
         self.window = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
         self.clock = pygame.time.Clock()
 
+        self.spawn_rate = 2000
+        self.point = 0
+
         self.targets = pygame.sprite.Group()
-        self.test = Target(self.rand_pos(), self.targets)
+    
+    def spawn_target(self):
+        Target(self.rand_pos(), self.targets)
     
     def rand_pos(self):
         x = random.randint(50, W-50)
@@ -44,17 +57,26 @@ class Game:
         sys.exit()
     
     def update(self):
-        self.targets.update()
+        for target in self.targets:
+            target.update()
+            if target.clicked:
+                target.kill()
+                self.point += 1
 
     def render(self):
         self.targets.draw(self.window)
     
     def run(self):
+        pygame.time.set_timer(SPAWNTARGET, self.spawn_rate)
         while True:
             self.clock.tick(FPS)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    print(self.point)
                     self.quit()
+                if event.type == SPAWNTARGET:
+                    self.spawn_target()
             
             self.update()
 
